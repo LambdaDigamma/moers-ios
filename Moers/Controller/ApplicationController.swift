de0e709b5f2c37be526a7b5424ad8b98001fc24a
Lennart Fischer <lennart.fischer@adolfinum.de>
2018-04-14 23:25:56 +0200

    Added ApplicationController

---
+++ Moers/Controller/ApplicationController.swift
@@ -0,0 +1,20 @@
+//
+//  ApplicationController.swift
+//  Moers
+//
+//  Created by Lennart Fischer on 14.04.18.
+//  Copyright © 2018 Lennart Fischer. All rights reserved.
+//
+
+import UIKit
+
+class ApplicationController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+
+}
