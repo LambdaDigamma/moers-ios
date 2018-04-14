bc3da12e56ad5793f635210fb347140c9ac4409e
Lennart Fischer <lennart.fischer@adolfinum.de>
2018-04-15 00:19:34 +0200

    Added TabBarController

---
+++ Moers/Controller/TabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  TabBarController.swift
+//  Moers
+//
+//  Created by Lennart Fischer on 15.04.18.
+//  Copyright © 2018 Lennart Fischer. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+
+}
