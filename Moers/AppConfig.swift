0565dc945255eeb9b4bdc53b3a12450a97d3fdfa
Lennart Fischer <lennart.fischer@adolfinum.de>
2018-04-14 23:25:04 +0200

    Added AppConfig

---
+++ Moers/AppConfig.swift
@@ -0,0 +1,17 @@
+//
+//  AppConfig.swift
+//  Moers
+//
+//  Created by Lennart Fischer on 14.04.18.
+//  Copyright © 2018 Lennart Fischer. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+struct AppConfig {
+    
+    let name: String
+    let coordinate: CLLocationDegrees
+    
+}
