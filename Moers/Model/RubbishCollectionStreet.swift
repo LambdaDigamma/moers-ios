b2b11cfac23ac50e105b40bbbc9997a969a8cec4
Lennart Fischer <lennart.fischer@adolfinum.de>
2018-04-19 21:27:36 +0200

    Added RubbishCollectionStreet

---
+++ Moers/Model/RubbishCollectionStreet.swift
@@ -0,0 +1,21 @@
+//
+//  RubbishCollectionStreet.swift
+//  Moers
+//
+//  Created by Lennart Fischer on 19.04.18.
+//  Copyright © 2018 Lennart Fischer. All rights reserved.
+//
+
+import Foundation
+
+struct RubbishCollectionStreet {
+    
+    var street: String
+    var residualWaste: Int
+    var organicWaste: Int
+    var paperWaste: Int
+    var yellowBag: Int
+    var greenWaste: Int
+    var sweeperDay: String
+    
+}
