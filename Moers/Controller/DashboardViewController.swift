ccb418a6fed276b6f8030857b5dd9837e2d9300f
Lennart Fischer <lennart.fischer@adolfinum.de>
2018-04-20 21:11:50 +0200

    Added DashboardViewController

---
+++ Moers/Controller/DashboardViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DashboardViewController.swift
+//  Moers
+//
+//  Created by Lennart Fischer on 20.04.18.
+//  Copyright © 2018 Lennart Fischer. All rights reserved.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+
+}
