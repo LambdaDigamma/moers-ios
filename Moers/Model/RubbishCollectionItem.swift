87cf7e97d7d5fc46b84aeefc9fa520d26e69dda6
Lennart Fischer <lennart.fischer@adolfinum.de>
2018-04-20 21:04:04 +0200

    Added RubbishCollectionItem Model

---
+++ Moers/Model/RubbishCollectionItem.swift
@@ -0,0 +1,16 @@
+//
+//  RubbishCollectionItem.swift
+//  Moers
+//
+//  Created by Lennart Fischer on 20.04.18.
+//  Copyright © 2018 Lennart Fischer. All rights reserved.
+//
+
+import Foundation
+
+struct RubbishCollectionItem {
+    
+    let date: String
+    let type: RubbishWasteType
+    
+}
