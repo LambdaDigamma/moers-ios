c7ec77c413e8b51c9603929f9b57733107765039
Lennart Fischer <lennart.fischer@adolfinum.de>
2018-05-19 15:21:10 +0200

    Added OtherViewController

---
+++ Moers/Controller/OtherViewController.swift
@@ -0,0 +1,20 @@
+//
+//  OtherViewController.swift
+//  Moers
+//
+//  Created by Lennart Fischer on 19.05.18.
+//  Copyright © 2018 Lennart Fischer. All rights reserved.
+//
+
+import UIKit
+
+class OtherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+}
