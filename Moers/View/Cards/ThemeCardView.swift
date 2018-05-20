235856dfb65df7821be9a2649edb5c6700dacf87
Lennart Fischer <lennart.fischer@adolfinum.de>
2018-05-20 22:53:10 +0200

    Added ThemeCardView

--- Moers/View/Cards/ThemeCardView.swift
+++ Moers/View/Cards/ThemeCardView.swift
@@ -7,15 +7,78 @@
 //
 
 import UIKit
+import Gestalt
 
 class ThemeCardView: TitleCardView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    public var theme: Theme? {
+        didSet {
+            
+            guard let theme = theme else { return }
+            
+            colorList.addArrangedSubview(buildColorView(color: theme.navigationBarColor))
+            colorList.addArrangedSubview(buildColorView(color: theme.backgroundColor))
+            colorList.addArrangedSubview(buildColorView(color: theme.accentColor))
+            colorList.addArrangedSubview(buildColorView(color: theme.decentColor))
+            
+        }
     }
-    */
 
+    lazy var colorList: UIStackView = {
+        
+        let stackView = UIStackView(arrangedSubviews: [])
+        
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.alignment = UIStackViewAlignment.fill
+        stackView.spacing = 8
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return stackView
+        
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.addSubview(colorList)
+        
+        self.setupConstraints()
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupConstraints() {
+        
+        let constraints = [colorList.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 16),
+                           colorList.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 16),
+                           colorList.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -16),
+                           colorList.heightAnchor.constraint(equalToConstant: 30),
+                           colorList.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -16)]
+        
+        NSLayoutConstraint.activate(constraints)
+        
+    }
+    
+    private func buildColorView(color: UIColor) -> UIView {
+        
+        let view = UIView()
+        
+        view.backgroundColor = color
+        view.layer.borderWidth = 2
+        view.layer.cornerRadius = 2
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        ThemeManager.default.apply(theme: Theme.self, to: view) { (themeable, theme) in
+            themeable.layer.borderColor = theme.decentColor.cgColor
+        }
+        
+        return view
+        
+    }
+    
 }
