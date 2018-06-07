9de197cdf38a0d04ff9bb62ba7dcc44b22d291d8
Lennart Fischer <lennart.fischer@adolfinum.de>
2018-06-06 17:43:35 -0700

    Added PetrolStationViewController

---
+++ Moers/Controller/PetrolStationViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PetrolStationViewController.swift
+//  Moers
+//
+//  Created by Lennart Fischer on 06.06.18.
+//  Copyright © 2018 Lennart Fischer. All rights reserved.
+//
+
+import UIKit
+
+class PetrolStationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+
+}
