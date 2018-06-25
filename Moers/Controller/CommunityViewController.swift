e737f01175e40789b57eb1d13fc1832e0a116295
Lennart Fischer <lennart.fischer@adolfinum.de>
2018-06-25 11:10:08 +0200

    Added CommunityViewController

---
+++ Moers/Controller/CommunityViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CommunityViewController.swift
+//  Moers
+//
+//  Created by Lennart Fischer on 25.06.18.
+//  Copyright © 2018 Lennart Fischer. All rights reserved.
+//
+
+import UIKit
+
+class CommunityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+    
+}
