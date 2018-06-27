17309a8c42e693cc9f0e0b4c1867c160300b2ab0
Lennart Fischer <lennart.fischer@adolfinum.de>
2018-06-27 23:17:19 +0200

    Added Regex String Operand Extension

--- Moers/Extensions.swift
+++ Moers/Extensions.swift
@@ -261,3 +261,11 @@ public struct Math {
     }
     
 }
+
+extension String {
+    static func ~= (lhs: String, rhs: String) -> Bool {
+        guard let regex = try? NSRegularExpression(pattern: rhs) else { return false }
+        let range = NSRange(location: 0, length: lhs.utf16.count)
+        return regex.firstMatch(in: lhs, options: [], range: range) != nil
+    }
+}
