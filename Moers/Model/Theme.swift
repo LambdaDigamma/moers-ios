150adabc37f5ded41576816f4814d6f3eec7ea04
Lennart Fischer <lennart.fischer@adolfinum.de>
2018-07-25 17:27:59 +0200

    Updated some Themes

--- Moers/Model/Theme.swift
+++ Moers/Model/Theme.swift
@@ -26,7 +26,7 @@ struct Theme: ThemeProtocol {
     static private let darkGray = UIColor(red: 0.137, green: 0.122, blue: 0.125, alpha: 1.00)
     static private let yellow = UIColor(red: 1.000, green: 0.949, blue: 0.200, alpha: 1.00)
     
-    static public let all: [Theme] = [lightning, dark]
+    static public let all: [Theme] = [lightning, dark, mono, red]
     
 //    static let light = Theme(color: UIColor.black,
 //                             backgroundColor: UIColor(red: 0.980, green: 0.980, blue: 0.980, alpha: 1.00),
@@ -50,6 +50,30 @@ struct Theme: ThemeProtocol {
                                  cardShadow: true,
                                  cardBackgroundColor: UIColor.white)
     
+    static let mono = Theme(identifier: "Mono",
+                            color: UIColor.black,
+                            backgroundColor: UIColor.white,
+                            navigationBarColor: UIColor.black,
+                            tabBarColor: UIColor.black,
+                            accentColor: UIColor.white,
+                            decentColor: UIColor.lightGray,
+                            separatorColor: UIColor.lightGray,
+                            statusBarStyle: .lightContent,
+                            cardShadow: true,
+                            cardBackgroundColor: UIColor.white)
+    
+    static let red = Theme(identifier: "Red",
+                          color: UIColor.black,
+                          backgroundColor: UIColor.white,
+                          navigationBarColor: UIColor(hexString: "#424242")!,
+                          tabBarColor: UIColor(hexString: "#424242")!,
+                          accentColor: UIColor.white,
+                          decentColor: UIColor.lightGray,
+                          separatorColor: UIColor.lightGray,
+                          statusBarStyle: .lightContent,
+                          cardShadow: true,
+                          cardBackgroundColor: UIColor.white)
+    
 //    static let lightJulius = Theme(color: UIColor(red: 0x2C, green: 0x28, blue: 0x29),
 //                                   backgroundColor: UIColor(hexString: "#000000")!,
 //                                   navigationBarColor: UIColor(red: 0xDC, green: 0x39, blue: 0x32),
@@ -66,6 +90,18 @@ struct Theme: ThemeProtocol {
                             navigationBarColor: UIColor.black,
                             tabBarColor: UIColor.black,
                             accentColor: yellow,
+                            decentColor: UIColor(red: 0x7F, green: 0x7F, blue: 0x7F),
+                            separatorColor: UIColor(red: 0.149, green: 0.196, blue: 0.220, alpha: 1.00),
+                            statusBarStyle: .lightContent,
+                            cardShadow: false,
+                            cardBackgroundColor: darkGray.darker(by: 2)!)
+    
+    static let darkOrange = Theme(identifier: "Dark Orange",
+                            color: UIColor.white,
+                            backgroundColor: darkGray,
+                            navigationBarColor: UIColor.black,
+                            tabBarColor: UIColor.black,
+                            accentColor: UIColor.orange,
                             decentColor: UIColor(red: 0x7F, green: 0x7F, blue: 0x7F),
                             separatorColor: UIColor(red: 0.149, green: 0.196, blue: 0.220, alpha: 1.00),
                             statusBarStyle: .lightContent,
