8c9dbc8ae16278a58077af042cf1a3ab43779a14
Lennart Fischer <lennart.fischer@adolfinum.de>
2018-08-14 22:47:08 +0200

    Added & Implemented EventManager

---
+++ Moers/Manager/EventManager.swift
@@ -0,0 +1,63 @@
+//
+//  EventManager.swift
+//  Moers
+//
+//  Created by Lennart Fischer on 14.08.18.
+//  Copyright © 2018 Lennart Fischer. All rights reserved.
+//
+
+import Foundation
+import Reachability
+
+public let baseURL = "http://localhost:8080/api/"
+
+struct EventManager {
+    
+    static var shared = EventManager()
+    
+    private let reachability = Reachability()!
+    private let session = URLSession.shared
+    
+    public func getEvents(completion: @escaping ((Error?, [Event]?) -> Void)) {
+        
+        if reachability.connection != .none {
+            
+            guard let url = URL(string: baseURL + "events") else { return }
+            
+            let request = URLRequest(url: url)
+            
+            session.dataTask(with: request) { (data, response, error) in
+                
+                if let error = error {
+                    completion(error, nil)
+                    return
+                }
+                
+                guard let data = data else {
+                    completion(APIError.noData, nil)
+                    return
+                }
+                
+                do {
+                    
+                    let decoder = JSONDecoder()
+                    
+                    decoder.keyDecodingStrategy = .convertFromSnakeCase
+                    
+                    let events = try decoder.decode([Event].self, from: data)
+                    
+                    completion(nil, events)
+                    
+                } catch {
+                    completion(error, nil)
+                }
+                
+            }.resume()
+            
+        } else {
+            completion(APIError.noConnection, nil)
+        }
+        
+    }
+    
+}
