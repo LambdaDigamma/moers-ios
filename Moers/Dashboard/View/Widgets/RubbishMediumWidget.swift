39834d716a27b30bbf9c29619b2e5b82330ae034
Lennart Fischer <github@lambdadigamma.com>
2021-04-28 15:26:04 +0200

    Added some Things

--- Moers/Dashboard/View/Widgets/RubbishMediumWidget.swift
+++ Moers/Dashboard/View/Widgets/RubbishMediumWidget.swift
@@ -7,15 +7,98 @@
 //
 
 import SwiftUI
+import WidgetKit
 
 struct RubbishMediumWidget: View {
+    
+    // TODO: Vielleicht die vier Dinger wie Kalenderblätter? (mit Umrandung)
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.systemBackground
+            
+            VStack(alignment: .leading) {
+                Text("Adlerstraße")
+                    .font(.title).fontWeight(.semibold)
+                
+                HStack() {
+                    
+                    VStack {
+                        
+                        Image("paperWaste")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(maxWidth: 80)
+                        
+                        Text("Morgen")
+                            .font(.footnote)
+                        
+                    }
+                    
+                    VStack {
+                        
+                        Image("yellowWaste")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(maxWidth: 80)
+                        
+                        Text("21.03.")
+                            .font(.footnote)
+                        
+                    }
+                    
+                    VStack {
+                        
+                        Image("greenWaste")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(maxWidth: 80)
+                        
+                        Text("24.03.")
+                            .font(.footnote)
+                        
+                    }
+                    
+                    VStack {
+                        
+                        Image("residualWaste")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(maxWidth: 80)
+                        
+                        Text("28.03.")
+                            .font(.footnote)
+                        
+                    }
+                    
+                    
+                    
+                }.frame(maxWidth: .infinity, alignment: .leading)
+            }
+            .padding()
+            .frame(maxWidth: .infinity,
+                   maxHeight: .infinity,
+                   alignment: .topLeading)
+            
+            
+            
+        }
+        
+        
+        
     }
 }
 
 struct RubbishMediumWidget_Previews: PreviewProvider {
     static var previews: some View {
-        RubbishMediumWidget()
+        
+        let widget = RubbishMediumWidget()
+            .previewLayout(.fixed(width: 360, height: 169))
+            
+        return Group {
+            widget
+            widget.environment(\.colorScheme, .dark)
+            
+        }
+        
     }
 }
