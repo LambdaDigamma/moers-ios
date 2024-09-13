546546320c3c52fe84789498aa26b68de1d8aaea
Lennart Fischer <github@lambdadigamma.com>
2024-09-14 00:07:51 +0200

    refactored dependencies
    Signed-off-by: Lennart Fischer <github@lambdadigamma.com>

--- modules/core/build.gradle.kts
+++ modules/core/build.gradle.kts
@@ -35,46 +35,33 @@ kotlin {
         }
         androidMain.dependencies {
 
-            implementation("androidx.core:core-ktx:1.13.1")
-            implementation("androidx.appcompat:appcompat:1.7.0")
-            implementation("com.google.android.material:material:1.12.0")
-
-            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
-
             // Google Play Services
-            implementation("com.google.android.gms:play-services-location:21.3.0")
-            implementation("androidx.work:work-runtime-ktx:2.9.1")
-
-            // Room Database
-            implementation("androidx.room:room-runtime:2.6.1")
-            implementation("androidx.room:room-ktx:2.6.1")
-            implementation("androidx.room:room-paging:2.6.1")
+            implementation(libs.play.services.location)
 
             // Hilt
-            implementation("com.google.dagger:hilt-android:2.40")
             implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
 
             // Retrofit
-            implementation("com.squareup.retrofit2:retrofit:2.9.0")
-            implementation("com.squareup.retrofit2:converter-simplexml:2.9.0")
-            implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-            implementation("com.squareup.retrofit2:retrofit-mock:2.9.0")
+            implementation(libs.retrofit)
+            implementation(libs.converter.simplexml)
+            implementation(libs.converter.gson)
+            implementation(libs.retrofit.mock)
 
             // Datastore
-            implementation("androidx.datastore:datastore:1.0.0")
-            implementation("androidx.datastore:datastore-preferences:1.0.0")
+            implementation(libs.androidx.datastore)
+            implementation(libs.androidx.datastore.preferences)
 
             // Compose
-            implementation("androidx.compose.ui:ui:1.7.1")
-            implementation("androidx.compose.material3:material3:1.3.0")
-            implementation("androidx.compose.ui:ui-tooling-preview:1.7.1")
-            implementation("androidx.compose.runtime:runtime-livedata:1.7.1")
+            implementation(libs.androidx.compose.ui)
+            implementation(libs.androidx.material3)
+            implementation(libs.androidx.compose.ui.tooling.preview)
+            implementation(libs.androidx.compose.runtime.livedata)
 
             // Google Maps
-            implementation("com.google.maps.android:maps-compose:6.1.2")
-            implementation("com.google.android.gms:play-services-maps:19.0.0")
+            implementation(libs.maps.compose)
+            implementation(libs.play.services.maps)
 
-            implementation("com.google.accompanist:accompanist-swiperefresh:0.24.13-rc")
+            implementation(libs.accompanist.swiperefresh)
 
         }
         commonTest.dependencies {
