8705ead95b99e833e52524c867714c402a551450
Lennart Fischer <info@lambdadigamma.com>
2026-05-06 11:34:45 +0200

    ci: fix
    Signed-off-by: Lennart Fischer <info@lambdadigamma.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,10 +18,6 @@ pluginManagement {
         kotlin("jvm") version "2.2.21"
     }
 }
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
-
 dependencyResolutionManagement {
     repositories {
         google {
